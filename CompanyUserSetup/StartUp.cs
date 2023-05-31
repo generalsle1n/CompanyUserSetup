baf5e074036cfa9f7380735584251a66728b2b44
generalsle1n <niels.schuler@web.de>
2023-05-31 19:33:44 +0200

    Renamed FIles

--- CompanyUserSetup/StartUp.cs
+++ CompanyUserSetup/StartUp.cs
@@ -1,6 +1,6 @@
 namespace CompanyUserSetup
 {
-    internal static class Program
+    internal static class StartUp
     {
         /// <summary>
         ///  The main entry point for the application.
@@ -11,7 +11,7 @@ namespace CompanyUserSetup
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainMenu());
         }
     }
 }
